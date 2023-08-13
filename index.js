af46c810a0bf264a0d5f747f7d1832c0add8958e
abcmehedi5 <abcmehedi5@gmail.com>
2023-08-13 11:51:30 +0600

    vercel diployment

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+// middleware
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Lang Master");
 });
 
-// MONGODB CONNECT
-
+// MONGODB CONNECT START------------------------------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mbjz2.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,19 +22,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+
+    // database collection
+    const db = client.db("langmaster");
+    const userCollection = db.collection("users");
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
+
+// MONGODB CONNECT END------------------------------------
 
 app.listen(port, () => {
   console.log(`Lang master app listening on port ${port}`);
