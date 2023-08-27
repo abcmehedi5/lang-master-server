33824ac104d89d45b4a0913c1c7881348c357a16
abcmehedi5 <abcmehedi5@gmail.com>
2023-08-28 00:52:40 +0600

    notification api

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -10,16 +10,27 @@ const createNotification = async (notificationData) => {
 };
 
 // get operation controller
-const getNotification = async () => {
+const getNotification = async (query) => {
   const client = await connectToMongoDB();
   const notificationCollection = client
-    .db("LangMaster") 
+    .db("LangMaster")
     .collection("notifications");
-  const notificationData = notificationCollection.find().toArray();
+  const notificationData = notificationCollection.find(query).toArray();
   return notificationData;
 };
 
+// delete notification
+
+const deleteNotification = async (query) => {
+  const client = await connectToMongoDB();
+  const notificationCollection = client
+    .db("LangMaster")
+    .collection("notifications");
+  const result = await notificationCollection.deleteOne(query);
+  return result;
+};
 module.exports = {
   createNotification,
   getNotification,
+  deleteNotification,
 };
--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const notificationController = require("../controllers/notificationController");
+const { ObjectId } = require("mongodb");
 
 // notificatin create oparation
 router.post("/notification", async (req, res) => {
   try {
     const notificationData = req.body;
+    console.log(notificationData);
     await notificationController.createNotification(notificationData);
     res.status(200).send({
       message: "Notification Create Successfull",
@@ -18,11 +20,32 @@ router.post("/notification", async (req, res) => {
 // notification post oparetion
 
 router.get("/notification", async (req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
   try {
-    const notificationData = await notificationController.getNotification();
+    const notificationData = await notificationController.getNotification(
+      query
+    );
     res.status(200).send(notificationData);
   } catch (error) {
     res.status(500).send({ error: "NOtification Internal Server Error" });
+  }
+});
+
+// delete notification
+
+router.delete("/notification/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const query = { _id: new ObjectId(id) };
+    const notificationDelete = await notificationController.deleteNotification(
+      query
+    );
+    res
+      .status(200)
+      .send({ message: "notification deleted", data: notificationDelete });
+  } catch (error) {
+    res.status(500).send({ error: "Notification Internal Server Error" });
   }
 });
 
