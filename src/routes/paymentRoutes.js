d92eef174411974506a99e053834757fa42d266a
Mostakimw <scientist.expert@gmail.com>
2023-08-31 18:37:16 +0600

    nodemailer added

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,7 +1,36 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 const stripe = require("stripe")(`${process.env.PAYMENT_SECRET_KEY}`);
 const paymentController = require("./../controllers/paymentController");
+
+// nodemailer function
+
+const sendMail = (emailData, emailAddress) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: emailAddress,
+    subject: emailData.subject,
+    html: `<p>${emailData.desc}</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+      // do something useful
+    }
+  });
+};
 
 // generate payment secret
 router.post("/create-payment-intent", async (req, res) => {
@@ -21,7 +50,17 @@ router.post("/create-payment-intent", async (req, res) => {
 router.post("/payment-info", async (req, res) => {
   try {
     const paymentInfo = req.body.paymentInfo;
+
     const result = await paymentController.savePaymentInfo(paymentInfo);
+    // send confirmation email to user
+    sendMail(
+      {
+        subject: "Payment Successful",
+        desc: `Payment Id: ${result?.insertedId}, Transaction Id: ${paymentInfo.transactionId}`,
+      },
+      paymentInfo?.email
+    );
+    // send confirmation email to host
     res.status(200).send(result);
   } catch (error) {
     res.status(500).send({ message: "error on storing data" });
