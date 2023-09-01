c84f831226650b249f4a2cf129600719c64ed852
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-09-01 16:57:23 +0600

    makeUser and makeAdmin is added

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const blogRoutes = require("./src/routes/blogRoutes");
 const feedbackRoutes = require("./src/routes/feedbackRoutes");
 const grammarRoutes = require("./src/routes/grammarRoutes");
 const quizRoutes = require("./src/routes/quizsRoutes");
+const adminRoutes = require("./src/routes/adminRoutes");
+const makeUserRoutes = require("./src/routes/makeUserRoutes");
 app.use(cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Lang Master is Running");
@@ -25,6 +28,8 @@ app.use("/blogs", blogRoutes);
 app.use("/reviews", feedbackRoutes);
 app.use("/grammars", grammarRoutes);
 app.use("/quizs", quizRoutes);
+app.use("/admins", adminRoutes);
+app.use("/makeUsers", makeUserRoutes);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
