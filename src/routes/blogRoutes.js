1223b491c85ec16696cbbbbe88fe79a1349d2bc6
abcmehedi5 <abcmehedi5@gmail.com>
2023-09-06 23:39:06 +0600

    blog route bug fixed

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -4,7 +4,7 @@ const blogController = require("../controllers/blogController");
 const { ObjectId } = require("mongodb");
 
 // get all blogs data
-router.get("/payment", async (req, res) => {
+router.get("/blog", async (req, res) => {
   try {
     const blogData = await blogController.getBlog();
     res.status(200).send(blogData);
@@ -28,7 +28,7 @@ router.get("/blog/:id", async (req, res) => {
 
 // create post api
 
-router.post("/payment", async (req, res) => {
+router.post("/blog", async (req, res) => {
   try {
     const data = req.body;
     await blogController.createBlog(data);
@@ -37,46 +37,4 @@ router.post("/payment", async (req, res) => {
     res.status(500).send({ error: "internal server error", error });
   }
 });
-
-
-// // Update the like count for a specific blog post
-router.patch("/blog/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-
-    // Get the current like count from the request body
-    const { like } = req.body;
-
-    // Update the blog post with the new like count
-    await blogController.updateLikeCount(query, like);
-
-    res.status(200).send({ message: "Like count updated successfully" });
-  } catch (error) {
-    res.status(500).send({ error: "Internal server error", error });
-  }
-});
-
-// Update the like count for a specific blog post and store user information
-router.put("/blog/:id/like", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-    const { username, email, liked, userImg } = req.body;
-    await blogController.updateLikeCount(query);
-
-    // Store user information in the blog post
-    await blogController.storeLikedUser(query, { username, email, liked, userImg });
-
-    res.status(200).send({ message: "Like count updated successfully" });
-  } catch (error) {
-    res.status(500).send({ error: "Internal server error", error });
-  }
-});
-
-
-
-
-
-
 module.exports = router;
